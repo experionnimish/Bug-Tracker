3e73c4a25e5230c60f27e0b69e10260081ae9641
Nimish <nimish.mathew@experionglobal.com>
2017-01-09 17:55:52 +0530

    Login complete

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var passportLocal = require('passport-local');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
+// var db = require('./dbconnect');
 
 var app = express();
 
@@ -20,12 +23,55 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new passportLocal.Strategy(function(username, password, done) {
-  if(username == password) {
-    done(null, { id: username, name : username });
-  } else {
-    done(null, null);
+var sequelize = new Sequelize('bug_tracker', 'root', 'ndm0150040', {
+
+  host: 'localhost',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
+});
+
+var Users = sequelize.define('users', {
+  user_id: Sequelize.INTEGER,
+  password: Sequelize.STRING
+});
+
+// Users.sync({force: true}).then(function () {
+//   // Table created
+//   return Users.create({
+//     user_id: '511',
+//     password: 'pass123'
+//   });
+// });
+
+passport.use(new passportLocal.Strategy(function(username, password, done) {
+  // if(username == password) {
+  //   done(null, { id: username, name : username });
+  // } else {
+  //   done(null, null);
+  // }
+
+    Users.findAll({
+        // attributes: ['user_id', 'password'],
+      where: {
+          user_id: username }
+    }).then(function(data) {
+        if(data[0]) {
+            if(data[0].dataValues.password == password) {
+                done(null, { id: data[0].dataValues.user_id, pass : data[0].dataValues.password });
+            }
+            else {
+                done(null, null);
+            }
+        }
+        else {
+            done(null, null);
+        }
+    });
+
 }));
 
 passport.serializeUser(function(user, done) {
--- dbconnect.js
+++ dbconnect.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('bug_tracker', 'root', 'ndm0150040', {
 
   host: 'localhost',
-  dialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
 
   pool: {
     max: 5,
@@ -11,26 +11,8 @@ var sequelize = new Sequelize('bug_tracker', 'root', 'ndm0150040', {
     idle: 10000
   }
 
+
+
 });
 
-function handle_database(dbquery) {
-
-    pool.getConnection(function(err,connection){
-        if (err) {
-          res.json({"code" : 100, "status" : "Error in database connection!"});
-          return;
-        }
-
-        connection.query(dbquery,function(err,rows){
-            connection.release();
-            if(!err) {
-                res.json(rows);
-            }
-        });
-
-        connection.on('error', function(err) {
-              res.json({"code" : 100, "status" : "Error in database connection!"});
-              return;
-        });
-  });
-}
+module.exports = sequelize;
